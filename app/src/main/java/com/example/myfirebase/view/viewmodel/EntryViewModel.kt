579fe5eb6bf90a224aff72eca0ca979642f5ee6f
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-01-02 19:51:58 +0700

    menambahkan class entry view model dan private function validasi input

---
+++ app/src/main/java/com/example/myfirebase/view/viewmodel/EntryViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.myfirebase.view.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.myfirebase.modeldata.DetailSiswa
+import com.example.myfirebase.modeldata.UIStateSiswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class EntryViewModel(
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() &&
+                    alamat.isNotBlank() &&
+                    telpon.isNotBlank()
+        }
+    }
