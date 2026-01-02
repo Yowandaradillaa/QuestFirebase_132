d9d3500d01bb2de84c330e2111e404a43c63fada
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-01-02 18:22:32 +0700

    menambahkan function detail siswa dan siswa dan data class uistatesiswa

--- app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
@@ -4,12 +4,21 @@ data class Siswa(
     val id : Long = 0,
     val nama : String = "",
     val alamat : String = "",
-    val telepon : String = ""
+    val telpon : String = ""
 )
 
 data class DetailSiswa(
     val id : Long = 0,
     val nama : String = "",
     val alamat : String = "",
-    val telepon: String = ""
+    val telpon: String = ""
 )
+
+fun DetailSiswa.toDataSiswa(): Siswa = Siswa(id, nama, alamat, telpon)
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(id, nama, alamat, telpon)
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
