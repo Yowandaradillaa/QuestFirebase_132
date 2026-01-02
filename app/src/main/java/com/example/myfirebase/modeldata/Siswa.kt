af855132924538875935129eea0b119d9f9789d2
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-01-02 18:18:37 +0700

    menambahkan data class siswa dan detail siswa

---
+++ app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
@@ -0,0 +1,15 @@
+package com.example.myfirebase.modeldata
+
+data class Siswa(
+    val id : Long = 0,
+    val nama : String = "",
+    val alamat : String = "",
+    val telepon : String = ""
+)
+
+data class DetailSiswa(
+    val id : Long = 0,
+    val nama : String = "",
+    val alamat : String = "",
+    val telepon: String = ""
+)
