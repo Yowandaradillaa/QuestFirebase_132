be3dd0045d7b235f7592c9d2c6118139f20864e6
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-01-02 18:47:41 +0700

    menambahkan import yang dibutuhkan

--- app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
@@ -1,5 +1,10 @@
 package com.example.myfirebase.repositori
 
+import android.util.Log
+import com.example.myfirebase.modeldata.Siswa
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
 interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
