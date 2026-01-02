dd521ce6fc106aa7fa391bdb9cb297fe913eb0b2
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-01-02 19:54:12 +0700

    menambahkan function update ui dan suspend function addsiswa

--- app/src/main/java/com/example/myfirebase/view/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myfirebase/view/viewmodel/EntryViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.myfirebase.view.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.myfirebase.modeldata.DetailSiswa
@@ -7,6 +8,7 @@ import com.example.myfirebase.modeldata.UIStateSiswa
 import com.example.myfirebase.repositori.RepositorySiswa
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import com.example.myfirebase.modeldata.toDataSiswa
 
 class EntryViewModel(
     private val repositorySiswa: RepositorySiswa
@@ -24,3 +26,25 @@ class EntryViewModel(
                     telpon.isNotBlank()
         }
     }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            try {
+                repositorySiswa.postDataSiswa(
+                    uiStateSiswa.detailSiswa.toDataSiswa()
+                )
+                Log.d("EntryViewModel", "Data berhasil disimpan")
+            } catch (e: Exception) {
+                Log.e("EntryViewModel", "Error saat menyimpan data: ${e.message}")
+                throw e // Re-throw untuk di-handle di UI
+            }
+        }
+    }
+}
